2f90860af694e45b6daf0f9e2dafc78571a70bc9
vsmaliakou <vsmaliakou@yandex.by>
2024-02-20 17:10:14 +0300

    INIT: Init react typescript project

---
+++ README.md
@@ -0,0 +1,1 @@
+[https://www.youtube.com/watch?v=OJ16BaPC6VI](https://www.youtube.com/watch?v=OJ16BaPC6VI)
---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function AboutPage() {
+  return (
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad aliquid dolorum numquam aspernatur ut molestias, assumenda tempora vitae enim sed dolor maxime ex veritatis aliquam fugiat voluptatum architecto minus nostrum. Adipisci maxime eaque tenetur sunt iure, exercitationem deleniti reiciendis iste ullam? A mollitia enim nam aliquid, soluta laborum placeat ipsum?</div>
+  )
+}
